387f3256ab322bdd8ec6c43f91e0a1260198d3ff
Flavio Moraes <flaviojoe@gmail.com>
2022-10-30 04:37:36 -0300

    atualizei o endpoint pra buscar os dados

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const ApiService = axios.create(
     {
-        baseURL: 'http://127.0.0.1:8000/',
+        // baseURL: 'http://127.0.0.1:8000/',
+        baseURL: 'https://teacher-aula.herokuapp.com/',
         headers: {
             'Content-Type': 'application/json'
         }
